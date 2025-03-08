883875db1d0e29e83f0a29a2bf53fd0f252bf630
Sannidhi Shetty <sannidhii.1609@gmail.com>
2025-03-08 08:51:40 +0530

    Delete components directory

--- components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./footer.module.css";
-export default function Footer({ completedTodos, totalTodos }) {
-  return (
-    <div className={styles.footer}>
-      <span className={styles.item}>completedTodos:{completedTodos}</span>
-      <span className={styles.item}>Total Todos:{totalTodos}</span>
-    </div>
-  );
-}
